801ea797ed5633c43f56786521df1552ede6cec7
adriancostasantiago <57681303+adriancostasantiago@users.noreply.github.com>
2021-01-12 14:39:34 -0300

    Add files via upload

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise')
 
 const connection = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'acessog10',
-    database: 'banco'
+    host: 'sql10.freemysqlhosting.net',
+    user: 'sql10386586',
+    password: 'ATnWLvv6H3',
+    database: 'sql10386586'
 })
 
 module.exports = connection
